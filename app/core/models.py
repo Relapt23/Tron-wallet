812e629d3a6e415a57280f434ce3115dcdcd551f
Laptev Roman <romashlapt@mail.ru>
2025-03-30 17:06:12 +0700

    edit_endpoints

--- app/core/models.py
+++ app/core/models.py
@@ -1,16 +1,19 @@
+from datetime import datetime
 from decimal import Decimal
 
-from sqlalchemy import Numeric
+from sqlalchemy import Numeric, DateTime, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
     pass
 
+
 class TronWallet(Base):
     __tablename__ = 'tron_wallet'
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    wallet_address: Mapped[str] = mapped_column(unique=True, nullable=False)
+    wallet_address: Mapped[str] = mapped_column(nullable=False)
     bandwidth: Mapped[int]
     energy: Mapped[int]
     balance: Mapped[Decimal] = mapped_column(Numeric(12, 6))
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False, server_default=func.now())
--- app/endpoints.py
+++ app/endpoints.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query, HTTPException
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import get_session
@@ -17,10 +18,38 @@ async def fetch_address_info(address_request: WalletRequest,
     trx_balance = client.get_account_balance(address)
     bandwidth = client.get_bandwidth(address)
     energy = account_info.get("account_resource", {}).get("energy_usage", 0)
-    wallet = TronWallet(wallet_address=address, bandwidth=bandwidth, balance=trx_balance, energy=energy)
+    wallet = TronWallet(wallet_address=address,
+                        bandwidth=bandwidth,
+                        balance=trx_balance,
+                        energy=energy
+                        )
     session.add(wallet)
     await session.commit()
     return {"address": address,
             "energy": energy,
             "balance": trx_balance,
-            "bandwidth": bandwidth}
+            "bandwidth": bandwidth
+            }
+
+
+@router.get("/items")
+async def get_wallet_list(session: AsyncSession = Depends(get_session),
+                          cursor: int | None = Query(None, ge=1),
+                          limit: int = Query(20, ge=0)):
+    query = await session.execute(select(TronWallet.id).where(TronWallet.id == cursor))
+    result = query.scalar_one_or_none()
+    if not result:
+        raise HTTPException(detail="cursor_not_found", status_code=404)
+
+    query = select(TronWallet).limit(limit + 1).order_by(TronWallet.id.desc())
+    if cursor:
+        query = query.where(TronWallet.id <= cursor)
+
+    result = await session.execute(query)
+    items = result.scalars().all()
+    if len(items) == limit + 1:
+        return {"items": items[:-1],
+                "next_cursor": items[-1].id}
+    else:
+        return {"items": items,
+                "next_cursor": None}
