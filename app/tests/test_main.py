ec55be0b9b352631d9aac5cec090fb3beb27acce
Laptev Roman <romashlapt@mail.ru>
2025-04-05 17:06:50 +0700

    update tests

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -2,13 +2,13 @@ from decimal import Decimal
 
 from dotenv import load_dotenv
 
-from app.core.models import WalletInfo
-
 load_dotenv(".env.test")
+
+from app.core.models import WalletInfo
 
 import pytest
 from httpx import AsyncClient, ASGITransport
-from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
 from sqlalchemy import select, delete
 from unittest.mock import AsyncMock
 from app.tron_service import TronService, get_tron_service, TronAddressInfo
@@ -16,33 +16,29 @@ from app.core.db import create_async_engine, Base, get_session
 from main import app
 
 
-@pytest.fixture(scope="session")
-async def test_engine():
+@pytest.fixture()
+async def test_session():
     engine = create_async_engine("sqlite+aiosqlite:///:memory:", connect_args={"check_same_thread": False})
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-    yield engine
-    await engine.dispose()
+    test_sess = async_sessionmaker(bind=engine, expire_on_commit=False)
 
-
-@pytest.fixture(scope="session")
-async def run_before_and_after_tests(test_engine):
-    async_sess = async_sessionmaker(bind=test_engine, expire_on_commit=False, autocommit=False, autoflush=False)
-
-    async def get_session_override():
-        async with async_sess() as session:
+    async def override_get_session() -> AsyncSession:
+        async with test_sess() as session:
             yield session
 
-    app.dependency_overrides[get_session] = get_session_override
-    yield
+    app.dependency_overrides[get_session] = override_get_session
 
+    yield test_sess
 
-@pytest.fixture
-async def client(run_before_and_after_tests):
+    await engine.dispose()
+    app.dependency_overrides.clear()
+
+@pytest.fixture()
+async def client(test_session):
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
         yield client
-
 
 @pytest.mark.asyncio
 async def test_fetch_address_info(client):
@@ -57,16 +53,8 @@ async def test_fetch_address_info(client):
     assert "balance" in data and isinstance(data["balance"], (int, float, Decimal))
     assert "bandwidth" in data and isinstance(data["bandwidth"], int)
 
-
 @pytest.mark.asyncio
-async def test_record_wallet_info_with_mock(client: AsyncClient, run_before_and_after_tests, test_engine):
-    async_session_maker = async_sessionmaker(bind=test_engine, expire_on_commit=False, autocommit=False,
-                                             autoflush=False)
-
-    async with async_session_maker() as session:
-        await session.execute(delete(WalletInfo))
-        await session.commit()
-
+async def test_record_wallet_info_with_mock(client: AsyncClient, test_session):
     mock_tron_service = AsyncMock(spec=TronService)
     mock_tron_service.get_address_info.return_value = TronAddressInfo(
         address="TR6NmBJLuCXpmK9G75FgbqJE87szz1RTSx",
@@ -74,17 +62,13 @@ async def test_record_wallet_info_with_mock(client: AsyncClient, run_before_and_
         energy=50,
         bandwidth=20
     )
-
     app.dependency_overrides[get_tron_service] = lambda: mock_tron_service
 
-    address_request = {"address": "TR6NmBJLuCXpmK9G75FgbqJE87szz1RTSx"}
-
-    response = await client.post("/address", json=address_request)
-
+    response = await client.post("/address", json={"address": "TR6NmBJLuCXpmK9G75FgbqJE87szz1RTSx"})
     assert response.status_code == 200
     data = response.json()
 
-    async with async_session_maker() as session:
+    async with test_session() as session:
         result = await session.execute(select(WalletInfo).where(WalletInfo.wallet_address == data["address"]))
         wallet = result.scalar_one_or_none()
 
@@ -94,4 +78,4 @@ async def test_record_wallet_info_with_mock(client: AsyncClient, run_before_and_
     assert wallet.energy == data["energy"]
     assert wallet.bandwidth == data["bandwidth"]
 
-    app.dependency_overrides.pop(get_tron_service)
+    app.dependency_overrides.pop(get_tron_service, None)
